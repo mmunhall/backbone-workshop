35747d866f4f711312d734fa4fd5d0ef74fd051b
Mike Munhall <munhall.mike@gmail.com>
2013-07-12 12:48:58 -0600

    Added route object.

--- js/application.js
+++ js/application.js
@@ -20,6 +20,24 @@ var app = {};
     });
 
     app.ContactApp = Backbone.Router.extend({
-
+        routes: {
+            "create": "create",
+            "edit/:id": "edit",
+            "*actions": "list"
+        },
+        initialize: function () {
+            Backbone.history.start();
+        },
+        list: function () {
+            console.log('list');
+        },
+        create: function () {
+            console.log('create');
+        },
+        edit: function (id) {
+            console.log('edit ' + id);
+        }
     });
 })()
+
+var runtime = new app.ContactApp();
