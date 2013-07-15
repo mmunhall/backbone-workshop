ccaa366f98182677cdf55084668d2ffbf42255d2
Mike Munhall <munhall.mike@gmail.com>
2013-07-15 14:56:14 -0600

    Edit and delete models.

--- js/application.js
+++ js/application.js
@@ -3,8 +3,12 @@ var app = {};
 (function () {
     "use strict";
 
-    app.Contact = Backbone.Model.extend({
+    _.extend(app, Backbone.Events);
 
+    app.Contact = Backbone.Model.extend({
+        validate: function () {
+            
+        }
     });
 
     app.ContactList = Backbone.Collection.extend({
@@ -17,35 +21,78 @@ var app = {};
 
     app.ContactRecordView = Backbone.View.extend({
         tagName: 'tr',
+        className: 'recordView',
         template: Handlebars.compile($('script#contactRecordViewTemplate').html()),
+        id: function () {
+            return 'recordView_' + this.model.get('id');
+        },
         initialize: function () {
             this.listenTo(this.model, 'change', this.render);
             this.listenTo(this.model, 'destroy', this.remove);
+        },
+        events: {
+            'click a.deleteLink': 'delete'
         },
         render: function () {
             console.log('recordView: render');
             this.$el.html(this.template(this.model.toJSON()));
             return this;
         },
-        remove: function () {
-            console.log('recordView: remove');
+        delete: function () {
+            console.log('recordView: delete');
+            this.model.destroy();
         }
     });
 
     app.ContactEditView = Backbone.View.extend({
-
+        tagName: 'tr',
+        className: 'hidden editView',
+        id: function () {
+            return 'editView_' + this.model.get('id');
+        },
+        template: Handlebars.compile($('script#contactRecordEditTemplate').html()),
+        initialize: function () {
+            this.listenTo(this.model, 'change', this.render);
+            this.listenTo(this.model, 'destroy', this.remove);
+        },
+        events: {
+            'click a': 'cancel',
+            'click input[type="button"]': 'save'
+        },
+        render: function () {
+            console.log('editView: render');
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        },
+        cancel: function (e) { // this would be better using a custom event.
+            console.log('editView: cancel');
+            e.preventDefault();
+            this.render();
+            app.trigger('edit:done');
+        },
+        save: function (e) {
+            console.log('editView: save');
+            e.preventDefault();
+            this.model.save({
+                lastName: this.$el.find('span:eq(0)').text(),
+                firstName: this.$el.find('span:eq(1)').text(),
+                email: this.$el.find('span:eq(2)').text(),
+                phone: this.$el.find('span:eq(3)').text()
+            });
+            app.trigger('edit:done');
+        }
     });
 
     app.ContactApp = Backbone.Router.extend({
         routes: {
             "create": "create",
             "edit/:id": "edit",
-            "delete/:id": "delete",
             "*actions": "list"
         },
         initialize: function () {
             this.contacts = new app.ContactList();
             this.listenTo(this.contacts, 'reset', this.addAll);
+            this.listenTo(app, 'edit:done', this.afterEdit);
             //this.contacts.fetch();
             this.contacts.reset([
                 {
@@ -73,9 +120,14 @@ var app = {};
         },
         edit: function (id) {
             console.log('route: edit ' + id);
+            $('tr#recordView_' + id).addClass('hidden');;
+            $('tr#editView_' + id).removeClass('hidden');
         },
-        delete: function (id) {
-            console.log('route: delete ' + id);
+        afterEdit: function () {
+            console.log('router: doneEditing');
+            $('tr.recordView').removeClass('hidden');
+            $('tr.editView').addClass('hidden');
+            this.navigate('/');
         },
         addAll: function () {
             console.log('router: addAll');
@@ -88,6 +140,10 @@ var app = {};
             recordView.render();
             editView.render();
             $('table tbody').append(recordView.el);
+            $('table tbody').append(editView.el);
+        },
+        cancel: function () {
+            console.log('router: cancel edit');
         }
     });
 })()
