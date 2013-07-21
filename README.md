7f79aff144cc17f80eabc422fae317156ec4528c
Mike Munhall <munhall.mike@gmail.com>
2013-07-21 16:21:40 -0600

    Added empty Backbone objects.

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Begin here. This branch contains the JavaScript dependencies, some CSS and an in
 workshop/add-dependencies
 -------------------------
 Create an application.js file with some bootstrap code to get started.
+
+workshop/create-first-objects
+-----------------------------
+We'll build the first Backbone objects we plan to use in our application.
--- js/application.js
+++ js/application.js
@@ -3,4 +3,27 @@ var app = {};
 (function () {
     "use strict";
 
+    app.Contact = Backbone.Model.extend({
+
+    });
+
+    app.ContactList = Backbone.Collection.extend({
+
+    });
+
+    app.ContactRecordView = Backbone.View.extend({
+
+    });
+
+    app.ContactEditView = Backbone.View.extend({
+
+    });
+
+    app.ContactApp = Backbone.Router.extend({
+
+    });
 })()
+
+$(function () {
+    var runtime = new app.ContactApp();
+})
